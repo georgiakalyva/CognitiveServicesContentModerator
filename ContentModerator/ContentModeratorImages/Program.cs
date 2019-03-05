a5bfeebe281efc2563c62c84e8f7c0db0a488e6a
Georgia Kalyva <georgia_k22@hotmail.com>
2019-03-05 17:22:38 +0200

    Update Program.cs

--- ContentModerator/ContentModeratorImages/Program.cs
+++ ContentModerator/ContentModeratorImages/Program.cs
@@ -9,6 +9,7 @@ namespace ContentModeratorImages
             Console.WriteLine("Hello World!");
             //http://codestories.gr/wp-content/uploads/2019/03/sample2.jpg
             //http://codestories.gr/wp-content/uploads/2019/03/sample1.jpeg
+            //https://docs.microsoft.com/en-us/azure/cognitive-services/content-moderator/image-moderation-quickstart-dotnet
         }
     }
 }
